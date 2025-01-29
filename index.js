219823ad19b9801d24309a59644e6425c888bb1e
thitiwat <tihti061@gmail.com>
2025-01-29 11:28:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/hello-world', (req, res) => {
+	res.send('hello world1');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`);
+});
